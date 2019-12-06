686322ce5adfc2bdedde86f2221eee26131dd860
Jeff Bezanson <jeff.bezanson@gmail.com>
2019-12-06 16:09:33 -0500

    fix #32775, fix #34023, issues showing `kw` and `parameters` exprs

--- src/julia-parser.scm
+++ src/julia-parser.scm
@@ -1683,10 +1683,7 @@
   (and (pair? lst) (pair? (car lst)) (eq? (caar lst) 'parameters)))
 
 (define (to-kws lst)
-  (map (lambda (x) (if (assignment? x)
-                       `(kw ,@(cdr x))
-                       x))
-       lst))
+  (map =-to-kw lst))
 
 ;; like parse-arglist, but with `for` parsed as a generator
 (define (parse-call-arglist s closer)
